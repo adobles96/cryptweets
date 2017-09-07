9c50449b51c212e9c5ccc4bb84ad4ff7678e15b4
Alejandro Dobles <alejandro.d.96@gmail.com>
2017-09-06 19:17:06 -0600

    first commit

---
+++ listener.py
@@ -0,0 +1,42 @@
+import tweepy
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import json
+from keys import *
+
+auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+auth.set_access_token(twitter_access_token, twitter_access_secret)
+
+api = tweepy.API(auth)
+
+coins = ['bitcoin', 'ethereum']
+
+class FreqListener(StreamListener):
+    """FreqListener extends StreamListener."""
+
+    def on_data(self, data):
+        try:
+            #with open('python.json', 'a') as f:
+            #    f.write(data)
+            print(json.loads(data)['text'])
+            return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status_code):
+        print('ERROR:', status_code)
+        if status_code == 420:
+            return False
+        return True
+
+if __name__ == '__main__':
+    twitter_stream = Stream(auth, FreqListener())
+    twitter_stream.filter(track=coins)
+
+
+# DATABASE DESIGN:
+# tweets table:
+# tweet consists of: id     date    text    user    coordinates     coin
+# users table:
+#

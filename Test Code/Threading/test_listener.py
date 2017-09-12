1fb61d74eff5c1e053faa59063698b9723c2923c
Luis Fernando Varela <lfvarela@stanford.edu>
2017-09-11 21:23:10 -0500

    Basic threading of manager/listener/handler working on Test Codes/Threading

---
+++ Test Code/Threading/test_listener.py
@@ -0,0 +1,21 @@
+import threading
+import time
+
+
+class TestListenerThread(threading.Thread):
+
+    def __init__(self, e_go, e_wait, group=None, target=None, name=None,
+                 args=(), *, daemon=None):
+        super().__init__(group=group, target=target, name=name,
+                         daemon=daemon)
+        self.args = args
+        self.e_go = e_go
+        self.e_wait = e_wait
+
+    def run(self):
+        while True:
+            if not self.e_go.is_set():
+                self.e_wait.set()
+                self.e_go.wait()
+            print("Listener running")
+            time.sleep(2)

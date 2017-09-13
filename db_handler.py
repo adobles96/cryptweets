5576acff5bf1663e1c28da66be26700103723235
Alejandro Dobles <alejandro.d.96@gmail.com>
2017-09-12 21:05:23 -0600

    db_handler v1.0 All methods pass basic tests. NOTE: db crashed during testing but it appears to have been a postgres problem and not a problem with the module. module remains to be tested on an ec2 instance.

--- db_handler.py
+++ db_handler.py
@@ -1,19 +1,20 @@
 import psycopg2 as pg
 
 class Handler:
-    TWEET_TABLE_TEMPLATE = '(id_str VARCHAR(20) NOT NULL, date_created DATE, text TEXT, user , language TEXT, coordinates, coins, witheld_in_countries)'
-    TWEET_KEYS = ''
 
-    def __init__(self, db_name, user, password=''):
+    tweet_table_template = '(id_str VARCHAR(20) NOT NULL, created_at TIME WITH TIME ZONE, text TEXT NOT NULL, user TEXT NOT NULL, retweets INTEGER, coins TEXT, language TEXT, lat REAL, long REAL, witheld_in_countries TEXT)'
+    # Possibly useful in the future: to check an existing table's template use the following query: SELECT (column_name, data_type) FROM information_schema.columns WHERE table_name = 'times_test';
+
+    def __init__(self, db_name, user, password='', host='localhost'):
         self.db_name = db_name
         self.user = user
         self.password = password
-        self.cur = None
-        self.con = None
+        self.host = host
+        self.cur = self.con = None
 
     def __enter__(self):
         try:
-            self.con = pg.connect(dbname=self.db_name, user=self.user, password=self.password)
+            self.con = pg.connect(dbname=self.db_name, user=self.user, password=self.password, host=self.host) #will possibly need to include host = 'localhost'
             self.cur = self.con.cursor()
         except pg.Error as e:
             # Perhaps I shouldn't except this error
@@ -26,6 +27,7 @@ class Handler:
         # with handler:
 
     def __exit__(self, *exc):
+        self.con.commit() # This saves all changes made during the current context
         self.cur.close()
         self.con.close()
 
@@ -34,12 +36,13 @@ class Handler:
             return not self.cur.closed
         return False
 
-    def create_table(self, table_name):
+    def create_table(self, table_name, table_template):
         '''Creates a table with the specified name and with columns as in the
         variable TWEET_TABLE_TEMPLATE.'''
         assert(self.cursor_is_active())
         try:
-            self.cur.execute('CREATE TABLE {} {};'.format(table_name, TWEET_TABLE_TEMPLATE))
+            self.cur.execute('CREATE TABLE {} {};'.format(table_name, table_template))
+            self.con.commit()
         except pg.Error as e:
             print('Table creation failed. Rolling back connection. ERROR:', e)
             # resets cursor, otherwise any future executes will generate an InternalError
@@ -50,19 +53,57 @@ class Handler:
         assert(self.cursor_is_active())
         try:
             self.cur.execute('DROP TABLE {};'.format(table_name))
+            self.con.commit()
         except pg.Error as e:
             print('Failed to drop table. Rolling back connection. ERROR:', e)
             # resets cursor, otherwise any future executes will generate an InternalError
             self.con.rollback()
 
-    def insert_tweet(self, table_name, tweet):
+    # def unpack_tweet(tweet):
+    #     '''Receives a tweet and unpacks the relevant information from the json.
+    #     It then returns a VALUES string in valid SQL format. IF ANY CHANGES ARE
+    #     MADE TO THE TWEETS TABLE BE SURE TO CHANGE THIS METHOD!'''
+
+    def insert_tweet(self, table_name, tweet, unpacker):
+        '''Uses the unpacker function to unpack the tweet (ie to take information
+        in tweet and put it in the proper SQL format) and inserts that into the
+        table specified by table name. It is important that the unpacker outputs
+        values in a format that matches the columns of the specified table.
+        IMPORTANT: changes to the db won't be commited until the context closes.
+        this means you won't be able to read what you've written within the same context.
+        To change this there's an easy but costly fix. Simply add self.con.commit()
+        after the execute statement.'''
         assert(self.cursor_is_active())
-        pass
 
-    def retrieve_days_tweets(self, table_name):
-        pass
-        # GENERATOR!
+        try:
+            self.cur.execute('INSERT INTO {} VALUES {}'.format(table_name, unpacker(tweet)))
+        except pg.Error as e:
+            print('Failed to insert tweet. Rolling back connection. ERROR:', e)
+            # resets cursor, otherwise any future executes will generate an InternalError
+            self.con.rollback()
 
-    def query(self, table_name, query):
-        pass
-        #generator
+    def retrieve_tweets(self, table_name): # This function might be superfluous, leaving it in nonetheless
+        '''A generator that retrieves all tweets in the specified table.'''
+        try:
+            self.cur.execute('SELECT * FROM {};'.format(table_name))
+        except pg.Error as e:
+            print('Failed to retrieve tweets. Rolling back connection. ERROR:', e)
+            # resets cursor, otherwise any future executes will generate an InternalError
+            self.con.rollback()
+        for result in self.cur:
+             yield result
+
+    def query(self, query): # This function might be a little dangerous, leaving it in nonetheless
+        '''A generator that executes whatever query and yields the esults one by
+        one. IMPORTANT: changes to the db won't be commited until the context closes.
+        this means you won't be able to read what you've written within the same context.
+        To change this there's an easy but costly fix. Simply add self.con.commit()
+        after the execute statement.'''
+        try:
+            self.cur.execute(query)
+        except pg.Error as e:
+            print('Query failed. Rolling back connection. ERROR:', e)
+            # resets cursor, otherwise any future executes will generate an InternalError
+            self.con.rollback()
+        for result in self.cur:
+            yield result
